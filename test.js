79a5585275299d6457fbf2d081cde12ee846e573
Glenn Vandeuren <vandeurenglenn@gmail.com>
2019-01-02 21:43:46 +0100

    :notes: update test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const MultiHDWallet = require('./');
 const { encode, decode } = require('bs58');
-const { mnemonic, bs58, multiWIF, hash, signature, publicKey, pub, address, chainCode } = {
+const { mnemonic, bs58, multiWIF, hash, signature, publicKey, pub, address, chainCode, encrypted } = {
 	mnemonic: 'noble section royal vintage space tool devote tag also fluid alone electric',
 	bs58: '5BycwKbGxPkhDsZrdDUcTgQf9exz41y6ASPMgbYigHxtVARLTmUd67zibeKDCNBzqzjdSmER7AHLH22nf7toiUxGpMCAEo8UYmyQKzfWfzcFUVfJ',
 	multiWIF: '12nSSi35DyW2iMZCUj1Nk2hLaP9S1uVSyziSvGT6uJQD9rNRvj87mXaacmtbePPNfSFjjmnzzJcYHsHTs',
@@ -9,11 +9,11 @@ const { mnemonic, bs58, multiWIF, hash, signature, publicKey, pub, address, chai
 	publicKey: '033f2870261a1f6e2a4a82ceb2032432c4fd606e818caab4ed9e8ec29f3c6d21ff',
 	signature: '12KN9pRvs96voUUS2SuTSwgfGCrTcpm8TyoaQN9Dx8aHKeYKV8QzJ8EXxreUrCeraSFwtPk8MUQSjP6cF1mpXQwUwcyV',
 	pub: '03eadd67d69c020878c1cd7b35e0c2bbb847b48edbbb9858468e8065dcb3106c1d',
-	address: '7K3pFGxGchyvW7LT2wpzVMZgHcRfmPRKXQKChPyY2GoQ5R9PSc'
+	address: '7K3pFGxGchyvW7LT2wpzVMZgHcRfmPRKXQKChPyY2GoQ5R9PSc',
+	encrypted: 'U2FsdGVkX18sZdsqa0bov0Ur0UGQwKe/IZJXAtyccj3mFS+D2sPH7wm5/ew3YdpDL5nBtBI0EJb83/iFL0Say9R+J8P2K4qbzg9SpMxfSrR76V/0pWGHZXkxNSKDJIb2G43TjmIw4OXsk0+Xiod+Mw=='
 };
 
 const before = tape => {
-	tape.plan(1);
 	return new MultiHDWallet('leofcoin:olivia');
 };
 /**
@@ -21,74 +21,60 @@ const before = tape => {
  * @module MultiHDWallet
  * @extends HDWallet
  */
-test('MultiHDWallet can generate', tape => {
-	const hdnode = before(tape);
-	const generated = hdnode.generate();
-	tape.ok(true);
-});
-
-test('MultiHDWallet can export to MultiWIF', tape => {
-	const hdnode = before(tape);
-	hdnode.load(bs58);
-	hdnode.export();
-	tape.equal(hdnode.export(), multiWIF);
-});
-
-test('MultiHDWallet can import from multiWif', tape => {
-	const hdnode = before(tape);
-	hdnode.import(multiWIF);
-	tape.equal(hdnode.export(), multiWIF);
-});
-
-test('MultiHDWallet can load from saved', tape => {
-	const hdnode = before(tape);
-	hdnode.load(bs58)
-	tape.equal(hdnode.save(), bs58);
-});
-
-test('MultiHDWallet can recover using mnemonic', tape => {
-	const hdnode = before(tape);
-	hdnode.recover(mnemonic);
-	tape.equal(hdnode.save(), bs58);
-});
-
-test('MultiHDWallet can sign', tape => {
-	const hdnode = before(tape);
-	hdnode.load(bs58);
-	const account = hdnode.account(0)
-	const external = account.external(0)
-	const neutered = external.neutered;
-	console.log(external.privateKey);
-	tape.equal(signature, external.sign(hash));
-});
-
-test('MultiHDWallet can verify', tape => {
+test('MultiWallet', tape => {
+	tape.plan(12);
 	let hdnode = before(tape);
+	const generated = hdnode.generate();
+	tape.ok(generated, 'generate wallet');
+
+	hdnode = before(tape);
 	hdnode.load(bs58);
-	const account = hdnode.account(0);
+	tape.equal(hdnode.export(), multiWIF, 'export to MultiWIF');
+
+	hdnode = before(tape);
+	hdnode.import(multiWIF);
+	tape.equal(hdnode.export(), multiWIF, 'import from multiWif');
+
+	hdnode = before(tape);
+	hdnode.load(bs58)
+	tape.equal(hdnode.save(), bs58, 'load from saved');
+
+	hdnode = before(tape);
+	hdnode.recover(mnemonic);
+	tape.equal(hdnode.save(), bs58, 'recover using mnemonic');
+
+	hdnode = before(tape);
+	hdnode.load(bs58);
+	let account = hdnode.account(0)
+	let external = account.external(0)
+	tape.equal(signature, external.sign(hash), 'sign');
+
+	hdnode = before(tape);
+	hdnode.load(bs58);
+	account = hdnode.account(0);
 	const neutered = account.external(0).neutered;
-	const verified = neutered.verify(signature, hash, publicKey);
-	tape.equal(verified, true);
-});
+	tape.equal(neutered.verify(signature, hash, publicKey), true, 'verify');
 
-test('bob can verify alice\'s signature using Alice\'s address', tape => {
-	const hdnode = before(tape);
+	hdnode = before(tape);
 	hdnode.fromAddress(address, null, 'leofcoin:olivia');
-	const verified = hdnode.verify(signature, hash);
-	tape.equal(verified, true);
-});
+	tape.equal(hdnode.verify(signature, hash), true, 'bob can verify alice\'s signature using Alice\'s address');
 
-test('bob can verify alice\'s signature using Alice\'s publicKey', tape => {
-	const hdnode = before(tape);
+	hdnode = before(tape);
 	hdnode.fromPublicKey(publicKey, null, 'leofcoin:olivia');
-	const verified = hdnode.verify(signature, hash);
-	tape.equal(verified, true);
-});
+	tape.equal(hdnode.verify(signature, hash), true, 'bob can verify alice\'s signature using Alice\'s publicKey');
 
-test('MultiHDWallet can create internal/external chains', tape => {
-	const hdnode = before(tape);
+	hdnode = before(tape);
 	hdnode.load(bs58);
-	const account = hdnode.account(0);
+	account = hdnode.account(0);
 
-	tape.notEqual(account.internal(0).publicKey, account.external(0).publicKey);
-})
+	tape.notEqual(account.internal(0).publicKey, account.external(0).publicKey, 'create internal/external chains');
+
+
+	hdnode = before(tape);
+	hdnode.lock('pasword', multiWIF)
+	tape.ok(hdnode.encrypted && !hdnode.publicKey, 'lock')
+
+	hdnode = before(tape);
+	hdnode.unlock('pasword', encrypted)
+	tape.equal(hdnode.multiWIF, multiWIF, 'unlock')
+});
